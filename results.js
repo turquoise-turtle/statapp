d19e28c16f9499d2f3e84f1c0bbb158247678bde
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2019-06-11 21:34:17 +1000

    Improve graphing logic and remove examples

--- results.js
+++ results.js
@@ -3,147 +3,6 @@
 function d(h,m) {
 	return new Date(new Date().setHours(h,m,0,0));
 }
-//scatter plot example from https://plot.ly/javascript/line-and-scatter/#line-and-scatter-plot
-var trace1 = {
-  //OG example code: x: [1, 2, 3, 4],
-  //try 1: x: ['01:01', '02:02', '03:03', '04:04'],
-  //try 2: x: ['01:01:00', '02:02:00', '03:03:00', '04:04:00'],
-  //try 3: x: ['Mon Jun 03 2019 01:01:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 02:02:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 03:03:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 04:04:00 (Australian Eastern Standard Time)'],
-  //try 4: (finally works if I use a direct javascript Date object)
-  x: [d(1,1), d(2,2), d(3,3), d(4,4)],
-  //y: [10, 15, 13, 17],
-  y: [ d(4,49), d(2,32), d(1,30), d(3,38) ],
-  mode: 'lines',
-  type: 'scatter'
-};
-
-var trace2 = {
-  //x: [2, 3, 4, 5],
-  //x: ['02:20', '03:30', '04:40', '05:50'],
-  //x: ['02:20:00', '03:30:00', '04:40:00', '05:50:00'],
-  //x: ['Mon Jun 03 2019 02:20:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 03:30:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 04:40:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 05:50:00 (Australian Eastern Standard Time)'],
-  x: [d(2,20), d(3,30), d(4,40), d(5,50)],
-  //y: [16, 5, 11, 9],
-  y: [d(1,30), d(2,32), d(3,38), d(4,49)],
-  mode: 'lines',
-  type: 'scatter'
-};
-
-var trace3 = {
-  //x: [1, 2, 3, 4],
-  //x: ['01:30', '02:32', '03:38', '04:49'],
-  //x: ['01:30:00', '02:32:00', '03:38:00', '04:49:00'],
-  //x: ['Mon Jun 03 2019 01:30:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 02:32:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 03:38:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 04:49:00 (Australian Eastern Standard Time)'],
-  x: [d(1,30), d(2,32), d(3,38), d(4,49)],
-  //y: [12, 9, 15, 12],
-  y: [d(4,40), d(2,20),  d(5,50), d(3,30) ],
-  mode: 'lines',
-  type: 'scatter'
-};
-
-var data = [trace1, trace2, trace3];
-//var data = [trace1];
-var layouthere = {
-	xaxis : {
-		tickformat: '%H:%M'
-	},
-	yaxis : {
-		tickformat: '%H:%M'
-	},
-	showlegend: true
-};
-Plotly.newPlot('drawHere', data, layouthere, {displayModeBar: true, displaylogo: false, responsive: true});
-
-
-
-//bar chart example from https://plot.ly/javascript/bar-charts/#colored-and-styled-bar-chart
-var trace1 = {
-  x: [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-  y: [219, 146, 112, 127, 124, 180, 236, 207, 236, 263, 350, 430, 474, 526, 488, 537, 500, 439],
-  name: 'Rest of world',
-  marker: {color: 'rgb(55, 83, 109)'},
-  type: 'bar'
-};
-
-var trace2 = {
-  x: [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-  y: [16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270, 299, 340, 403, 549, 499],
-  name: 'China',
-  marker: {color: 'rgb(26, 118, 255)'},
-  type: 'bar'
-};
-
-var data = [trace1, trace2];
-
-var layout = {
-  title: 'US Export of Plastic Scrap',
-  xaxis: {tickfont: {
-      size: 14,
-      color: 'rgb(107, 107, 107)'
-    }},
-  yaxis: {
-    title: 'USD (millions)',
-    titlefont: {
-      size: 16,
-      color: 'rgb(107, 107, 107)'
-    },
-    tickfont: {
-      size: 14,
-      color: 'rgb(107, 107, 107)'
-    }
-  },
-  legend: {
-    x: 0,
-    y: 1.0,
-    bgcolor: 'rgba(255, 255, 255, 0)',
-    bordercolor: 'rgba(255, 255, 255, 0)'
-  },
-  barmode: 'group',
-  bargap: 0.15,
-  bargroupgap: 0.1
-};
-
-Plotly.newPlot('myDiv2', data, layout, {responsive: true});
-
-//box plot example from https://plot.ly/javascript/box-plots/#grouped-box-plot
-var x = ['day 1', 'day 1', 'day 1', 'day 1', 'day 1', 'day 1',
-         'day 2', 'day 2', 'day 2', 'day 2', 'day 2', 'day 2']
-
-var trace1 = {
-  y: [0.2, 0.2, 0.6, 1.0, 0.5, 0.4, 0.2, 0.7, 0.9, 0.1, 0.5, 0.3],
-  x: x,
-  name: 'kale',
-  marker: {color: '#3D9970'},
-  type: 'box'
-};
-
-var trace2 = {
-  y: [0.6, 0.7, 0.3, 0.6, 0.0, 0.5, 0.7, 0.9, 0.5, 0.8, 0.7, 0.2],
-  x: x,
-  name: 'radishes',
-  marker: {color: '#FF4136'},
-  type: 'box'
-};
-
-var trace3 = {
-  y: [0.1, 0.3, 0.1, 0.9, 0.6, 0.6, 0.9, 1.0, 0.3, 0.6, 0.8, 0.5],
-  x: x,
-  name: 'carrots',
-  marker: {color: '#FF851B'},
-  type: 'box'
-};
-
-var data = [trace1, trace2, trace3];
-
-var layout = {
-  yaxis: {
-    title: 'normalized moisture',
-    zeroline: false
-  },
-  boxmode: 'group'
-};
-
-Plotly.newPlot('myDiv3', data, layout, {responsive: true});
 
 
 
@@ -161,33 +20,66 @@ db.info().then(function(){
 	
 	sa.l(dataset);
 	
+	graph_everything();
+});
+
+sa.el('#graphType').addEventListener('change', graph_everything);
+
+function graph_everything() {
 	var graphDataset = [];
+	var layout = {
+		showlegend: true
+	};
 	Object.keys(dataset).forEach(function(subtopic) {
 		sa.l(subtopic, dataset[subtopic]);
 		var currentAxisDataset = {
-			name: subtopic
+			name: subtopic//,
+// 			line: {shape: 'linear'}
+// 			line: {shape: 'spline'}
+// 			line: {shape: 'hv'}
+// 			line: {shape: 'vh'}
+// 			line: {shape: 'hvh'}
+// 			line: {shape: 'vhv'}
+// 			line: {dash: 'solid',width: 4}
+// 			line: {dash: 'dashdot',width: 4}
+// 			line: {dash: 'dot',width: 4}
+			
 		};
-		currentAxisDataset.x = dataset_to_dataset(dataset[subtopic][0], 'x');
-		currentAxisDataset.y = dataset_to_dataset(dataset[subtopic][1], 'y');
+		
 		
 		switch (sa.el('#graphType').value) {
 			case 'scatter':
-				currentAxisDataset.mode = 'markers'//'lines+markers';
-				currentAxisDataset.type = 'scatter';//scatter
+				currentAxisDataset.x = dataset_to_dataset(dataset[subtopic][0], 'x');
+				currentAxisDataset.y = dataset_to_dataset(dataset[subtopic][1], 'y');
+				currentAxisDataset.mode = 'lines+markers'//'lines+markers';
+				currentAxisDataset.type = 'scatter';
 				break;
+			case 'box':
+				currentAxisDataset.y = dataset_to_dataset(dataset[subtopic][1], 'y');
+				currentAxisDataset.type = 'box';
+				break;
+// 			case 'histogram':
+// 				currentAxisDataset.type = 'histogram';
+// 				currentAxisDataset.histfunc = 'avg';
+// 				currentAxisDataset.x = dataset_to_dataset(dataset[subtopic][0], 'x');
+// 				currentAxisDataset.y = dataset_to_dataset(dataset[subtopic][1], 'y');
+// 				break;
 		}
 		
 		graphDataset.push(currentAxisDataset);
 	});
 	sa.l(graphDataset);
 	
-	var layout = {
-		showlegend: true
-	};
+	
 	for (var xOrY of ['x', 'y']) {
 		if (metadata[xOrY + 'Type'] === 'time') {
 			layout[xOrY + 'axis'] = {
-				tickformat: '%H:%M'
+				tickformat: '%H:%M',
+				title: metadata[xOrY + 'Name']
+			}
+		} else {
+			layout[xOrY + 'axis'] = {
+				title: metadata[xOrY + 'Name']
 			}
 		}
 	}
@@ -199,26 +91,26 @@ db.info().then(function(){
 	}
 	
 	Plotly.newPlot('graphHere', graphDataset, layout, options);
-});
-/*
-var layouthere = {
-	xaxis : {
-		tickformat: '%H:%M'
-	},
-	yaxis : {
-		tickformat: '%H:%M'
-	},
-	showlegend: true
-};
-Plotly.newPlot('drawHere', data, layouthere, {displayModeBar: true, displaylogo: false, responsive: true});
-*/
+}
+
 function dataset_to_dataset(dataArray, xOrY) {
 	var newList = [];
 	for (var el of dataArray) {
 		if (metadata[xOrY + 'Type'] === 'time') {
 			el = el.substr(1);
 			var time = el.split(':');
+			//it took ages to figure out to use a direct javascript Date object, rather than strings or numbers
+			//like, here's a snippet of code from testing (that was showcased in a gif)
+			/*
+				OG example code: x: [1, 2, 3, 4],
+				try 1: x: ['01:01', '02:02', '03:03', '04:04'],
+				try 2: x: ['01:01:00', '02:02:00', '03:03:00', '04:04:00'],
+				try 3: x: ['Mon Jun 03 2019 01:01:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 02:02:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 03:03:00 (Australian Eastern Standard Time)', 'Mon Jun 03 2019 04:04:00 (Australian Eastern Standard Time)'],
+				try 4: (finally works if I use a direct javascript Date object, which I make with a helper function called d)
+					x: [d(1,1), d(2,2), d(3,3), d(4,4)],
+			*/
 			var realTime = d(time[0],time[1]);
+			
 			newList.push(realTime);
 		} else {
 			el = el.substr(1);
@@ -226,6 +118,12 @@ function dataset_to_dataset(dataArray, xOrY) {
 		}
 		//sa.l(el);
 	}
-	sa.l(newList);
+	//sa.l(newList);
 	return newList;
 }
+
+
+
+//scatter plot example from https://plot.ly/javascript/line-and-scatter/#line-and-scatter-plot
+//bar chart example from https://plot.ly/javascript/bar-charts/#colored-and-styled-bar-chart
+//box plot example from https://plot.ly/javascript/box-plots/#grouped-box-plot
